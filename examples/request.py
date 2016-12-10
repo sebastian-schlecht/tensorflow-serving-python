ec4758b37a5cc3f430f32765a67cbab981cd0a24
Sebastian Schlecht <mail@sebastian-schlecht.de>
2016-12-10 16:21:54 +1000

    Example cmd args and images

--- examples/request.py
+++ examples/request.py
@@ -1,15 +1,18 @@
 import argparse
+import pprint
 
 from tensorflow_serving_python.client import TFClient
+
+pp = pprint.PrettyPrinter(indent=2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='RPC Test.')
 
     parser.add_argument('--host', required=True, type=str, help='Hostname to query')
     parser.add_argument('--port', required=True, type=str, help='Port to query')
-
+    parser.add_argument('--image', required=True, type=str, help='Image to send (JPG format)')
     args = parser.parse_args()
 
-    data = open("cat.jpg", "rb").read()
+    data = open(args.image, "rb").read()
     client = TFClient(args.host, args.port)
-    print client.make_prediction(data, timeout=10)
+    pp.pprint(client.make_prediction(data, timeout=10))
